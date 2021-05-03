d61b84dc2b0c6d481054c73b33415eff4aa20e11
1812329085 <1812329085@qq.com>
2021-05-03 20:15:30 +0800

    建立分支

---
+++ demo-mall-service/src/main/java/com/demo/service/DemoMallServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMallServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMallServiceApplication.class, args);
+    }
+
+}
